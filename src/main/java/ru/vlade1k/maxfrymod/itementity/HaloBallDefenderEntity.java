3dc37407f9df3f25385ec7fe0310b79d91625f95
vlade1k <vladode2015@yandex.ru>
2025-09-30 00:15:38 +0300

    add attacking logic to halo ball

--- src/main/java/ru/vlade1k/maxfrymod/itementity/HaloBallDefenderEntity.java
+++ src/main/java/ru/vlade1k/maxfrymod/itementity/HaloBallDefenderEntity.java
@@ -4,19 +4,25 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.FlyingItemEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.data.DataTracker.Builder;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.World.ExplosionSourceType;
 import ru.vlade1k.maxfrymod.item.ItemRegistryManager;
 
 public class HaloBallDefenderEntity extends Entity implements FlyingItemEntity {
   private World world;
   private PlayerEntity owner;
+  private Entity enemy;
   private static final double RADIUS = 0.7;
-  private static final double THETA_DELTA = 0.05;
+  private static final double THETA_DELTA = 0.3;
   private double xCurrCoord;
   private double yCurrCoord;
   private double zCurrCoord;
@@ -59,10 +65,40 @@ public class HaloBallDefenderEntity extends Entity implements FlyingItemEntity {
           this.theta += THETA_DELTA;
         }
 
-        setPos(this.xCurrCoord, this.yCurrCoord, this.zCurrCoord);
+        this.setPos(this.xCurrCoord, this.yCurrCoord, this.zCurrCoord);
 
         if (currentTick == ALIVE_TIME) {
           discard();
+        }
+
+        var box = Box.from(Vec3d.of(this.owner.getBlockPos())).expand(10, 0, 10);
+        var badEntity = this.world.getOtherEntities(this.owner, box).stream()
+                                                                    .filter(entity -> entity instanceof HostileEntity)
+                                                                    .findFirst();
+
+        if (badEntity.isPresent()) {
+          this.enemy = badEntity.get();
+          this.isAttackMod = true;
+        }
+      } else {
+        var currentPos = new Vec3d(this.getX(), this.getY(), this.getZ());
+        var enemyPos = new Vec3d(enemy.getX(), enemy.getY(), enemy.getZ());
+        var directionVector = enemyPos.subtract(currentPos);
+        var normalizedDirection = directionVector.normalize();
+
+        currentPos = currentPos.add(normalizedDirection.multiply(0.2));
+        this.setPos(currentPos.getX(), currentPos.getY(), currentPos.getZ());
+
+        if (enemy.getBoundingBox().intersects(currentPos, currentPos.add(3))) {
+          this.world.createExplosion(
+              owner,
+              enemy.getX() + 0.1,
+              enemy.getEyeY() + 0.1,
+              enemy.getZ() + 0.1,
+              10,
+              ExplosionSourceType.TRIGGER
+          );
+          this.discard();
         }
       }
     }
